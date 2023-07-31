0036d2d89abe1f983eeea2cf2ce9c8fb47d957c4
codeship89 <ngchwanlii@codeship.app>
2023-07-31 10:49:39 +0800

    added platform guard

--- src/routes/api/users/+server.ts
+++ src/routes/api/users/+server.ts
@@ -1,8 +1,11 @@
 import type { RequestHandler } from "@sveltejs/kit";
 
 export const GET: RequestHandler = async function({ request, platform }) {    
-  let result = await platform?.env.DB.prepare(
-    "SELECT * FROM users LIMIT 5"
-  ).run();
-  return new Response(JSON.stringify(result));
+  if (platform) {
+    let result = await platform.env.DB.prepare(
+      "SELECT * FROM users LIMIT 5"
+    ).run();
+    return new Response(JSON.stringify(result));
+  }
+  return new Response("not found")
 }
