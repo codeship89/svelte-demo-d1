3c46e90db5f4d085f55a9224f076cc818a4f7d01
codeship89 <ngchwanlii@codeship.app>
2023-07-31 10:46:00 +0800

    added demo

--- src/routes/api/users/+server.ts
+++ src/routes/api/users/+server.ts
@@ -1,7 +1,7 @@
 import type { RequestHandler } from "@sveltejs/kit";
 
-export const GET: RequestHandler = async function({ request, platform }) {
-  let result = await platform.env.DB.prepare(
+export const GET: RequestHandler = async function({ request, platform }) {    
+  let result = await platform?.env.DB.prepare(
     "SELECT * FROM users LIMIT 5"
   ).run();
   return new Response(JSON.stringify(result));
